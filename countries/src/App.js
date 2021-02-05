93a5cda61345dd87f73e81e38a9716017b13659e
Kitty BRANCOURT <k.brancourt@gmail.com>
2021-02-05 12:06:31 +0100

    countries: maj

--- countries/src/App.js
+++ countries/src/App.js
@@ -12,14 +12,13 @@ export class App extends React.Component {
       capital:"",
       flag: "",
       population: "",
-      region:""
+      region:"",
+      isSelected: false
     }
 
     this.componentDidMount =this.componentDidMount.bind(this);
     this.getCountry =this.getCountry.bind(this);
-    this.selectedFrance=this.selectedFrance.bind(this);
-      this.selectedBrazil=this.selectedBrazil.bind(this);
-      this.selectedCroatia=this.selectedCroatia.bind(this);
+    
   }
 
    componentDidMount(){
@@ -27,50 +26,38 @@ export class App extends React.Component {
      .then(resultat => resultat.json())
     //  console.log("resultat", resultat)
      .then(name => {
-       this.setState={
+       this.setState({
             name: name[0].name,
             capital:name[0].capital,
             flag: name[0].flag,
             population: name[0].population,
             region:name[0].region
-     }
+     })
    })
    .catch(error => console.error(error))
   }
 
 
   getCountry(country){
-    fetch("https://restcountries.eu/rest/v2/name", country)
+    if(country === "france"){this.setState({name: "france"}); this.setState({isSelected:true}); console.log("is selected", this.state.isSelected)}
+    if(country === "brazil"){this.setState({name: "brazil"}); this.setState({isSelected:true}); console.log("is selected", this.state.isSelected)}
+    if(country === "croatia"){this.setState({name: "croatia"}); this.setState({isSelected:true}); console.log("is selected", this.state.isSelected)}
+
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(resultat => resultat.json())
      .then(name => {
-       this.setState={
+       this.setState({
         name: name[0].name,
         capital:name[0].capital,
         flag: name[0].flag,
         population: name[0].population,
         region:name[0].region
-     }
+     })
    })
    .catch(error => console.error(error))
   }
 
-  selectedFrance(){
-    this.setState({
-      name: "france"
-    })
-  } 
-
-  selectedBrazil(){
-    this.setState({
-      name: "brazil"
-    })
-  }
-  
-  selectedCroatia(){
-    this.setState({
-      name: "croatia"
-    })
-  }
+ 
 
   render(){
      
@@ -80,16 +67,28 @@ export class App extends React.Component {
         <h1>Countries</h1>
           <div className="row">
             <div className="col-3">
-              <Button type="button" isSelected={this.state.name === "france"?"form-control btn btn-primary" : "form-control btn btn-light"} onClick={this.selectedFrance}>France</Button>
+              <Button type="button" isSelected={this.state.name === "france"?"form-control btn btn-primary" : "form-control btn btn-light"} onClick={() => this.getCountry("france")}>France</Button>
             </div>
             <div className="col-3">
-              <Button type="button" isSelected={this.state.name === "brazil"?"form-control btn btn-primary" : "form-control btn btn-light"} onClick={this.selectedBrazil}>Brazil</Button>
+              <Button type="button" isSelected={this.state.name === "brazil"?"form-control btn btn-primary" : "form-control btn btn-light"} onClick={() => this.getCountry("brazil")}>Brazil</Button>
             </div>
             <div className="col-3">
-              <Button type="button" isSelected={this.state.name === "croatia"?"form-control btn btn-primary" : "form-control btn btn-light"} onClick={this.selectedCroatia}>Croatia</Button>
+              <Button type="button" isSelected={this.state.name === "croatia"?"form-control btn btn-primary" : "form-control btn btn-light"} onClick={() => this.getCountry("croatia")}>Croatia</Button>
+            </div>
+            <div >
+              <Cards ></Cards>
+            </div>
+            <div className="my-5 px-5 mx-auto"  style={{width: '200px'},{heigth: '250px'}}>
+              <img className="img-fluid" src={this.state.flag}/>
+              <div>{this.state.name}</div>
+              <div>{this.state.capital}</div>
+              <div>{this.state.population} habitants</div>
+              <div>{this.state.region}</div>
             </div>
           </div>
-        <Cards></Cards>
+          
+          
+          
       </div>
     );
   }
