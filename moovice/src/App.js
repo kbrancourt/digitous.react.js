20a01db6ba56711a8e2b2a2f95f00b666b5ea8d4
Kitty BRANCOURT <k.brancourt@gmail.com>
2021-02-11 11:57:33 +0100

    moovice partie 2

--- moovice/src/App.js
+++ moovice/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom"; // on importe les outils du router
-import results from './results'
 import Homepage from './Components/Homepage';
 import Weekly from './Components/Weekly';
 import WeeklyBattle from './Components/Weekly-Battle';
@@ -9,17 +8,46 @@ import PopularBattle from './Components/Popular-Battle';
 import Favorites from './Components/Favorites';
 
 class App extends React.Component{
+  constructor(){
+    super();
+      this.state={
+        detailsMovie:[],
+        title: "",
+        release_date:"",
+        original_language:"",
+        overview:"",
+        poster_path:"",
+        popularity:""
+      }
 
+  }
+
+  componentDidMount(){
+    this.getMovie("Wonder Woman");
+  }
+
+  getMovie(movie){
+
+    fetch("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9bdc573f40963087e0f47872b51195a9")
+    .then((response) => response.json())
+     .then((response) => {
+       console.log("result", response)
+       this.setState({
+        detailsMovie: response.results
+     })
+   })
+   .catch(error => console.error(error))
+  }
 
   render(){
     return (
       <BrowserRouter>
           <nav class="navbar navbar-light bg-light">
             <div class="container-fluid">
-              {results.map((movie) => {
+              {this.state.detailsMovie.map((movie) => {
                 return (
                   <span class="navbar-brand mb-0 h1">
-                    <Link to={`/results/${results.id}`}>{results.title}</Link>
+                    <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
                   </span>
                 );
               })}
